d65bcc9bbacaac7877ed5cea80644d4abff3245e
AbhiTsong <abhisheksubba88@gmail.com>
2020-08-29 21:09:17 +0530

    Small Change

--- src/components/MainPage/AllPosts/AllPosts.Component.jsx
+++ src/components/MainPage/AllPosts/AllPosts.Component.jsx
@@ -16,111 +16,117 @@ function AllPostsComponent() {
   let allPost = useSelector(postSelector);
   let [posts, setPosts] = useState([]);
 
+  // console.log("allPostallPostallPostallPost", allPost);
+
   useEffect(() => {
     dispatch(fetchAllPosts());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [dispatch]);
 
-  useEffect(() => {
-    if (!allPost.loading && allPost.allPosts) {
-      setPosts(allPost.allPosts.data.reverse());
-    }
-  }, []);
+  if (allPost.loading) {
+    return "Loading...";
+  }
 
   return (
     <div className="All_Post_Container">
-      {posts.map((post, idx) => (
-        <div key={post+idx} className="Single_Post_Container">
-          <div className="Image_Input_Container">
-            <img
-              className="UserProfilePic"
-              src={Logo}
-              alt="Creator Profile Pic"
-            />
-
-            <div className="Name_And_Dots_Container">
-              <h6>Creator Name</h6>
-              <h3>...</h3>
-            </div>
-          </div>
-          <div className="Single_Post_Content">
-            <div className="Center_Post_Text">
-              <p className="Single_Post_Text">{post.description}</p>
-            </div>
-          </div>
-          <div className="Comments_Container">
-            <div className="Reaction_Count">
-              <span role="img" aria-label="Post Reaction">
-                &#128525;
-              </span>
-              <div className="Comment_People">
-                <span>Freind 1, Friend 2</span>
-                <span>9 Comments</span>
-              </div>
-            </div>
-            <div className="Like_Comment_Icons_Container">
-              <span role="img" aria-label="Like">
-                &#128077; Like
-              </span>
-              <span className="Icon_Container">
+      {allPost.allPosts.data &&
+        allPost.allPosts.data
+          .map((post, idx) => (
+            <div key={post + idx} className="Single_Post_Container">
+              <div className="Image_Input_Container">
                 <img
-                  src={Comment}
-                  className="Comment_Icon"
-                  alt="Comment Icon"
-                />{" "}
-                Comment
-              </span>
-              <span className="Icon_Container">
-                <img src={Share} className="Share_Icon" alt="Share Icon" />{" "}
-                Share
-              </span>
-            </div>
-            <div className="All_Comments_Container">
-              <p>View 4 more comments</p>
-              <div className="Individual_Comment_Container">
-                <div className="Comments_Cotent">
+                  className="UserProfilePic"
+                  src={Logo}
+                  alt="Creator Profile Pic"
+                />
+
+                <div className="Name_And_Dots_Container">
+                  <h6>Creator Name</h6>
+                  <h3>...</h3>
+                </div>
+              </div>
+              <div className="Single_Post_Content">
+                <div className="Center_Post_Text">
+                  <p className="Single_Post_Text">{post.description}</p>
+                </div>
+              </div>
+              <div className="Comments_Container">
+                <div className="Reaction_Count">
+                  <span role="img" aria-label="Post Reaction">
+                    &#128525;
+                  </span>
+                  <div className="Comment_People">
+                    <span>Freind 1, Friend 2</span>
+                    <span>9 Comments</span>
+                  </div>
+                </div>
+                <div className="Like_Comment_Icons_Container">
+                  <span role="img" aria-label="Like">
+                    &#128077; Like
+                  </span>
+                  <span className="Icon_Container">
+                    <img
+                      src={Comment}
+                      className="Comment_Icon"
+                      alt="Comment Icon"
+                    />{" "}
+                    Comment
+                  </span>
+                  <span className="Icon_Container">
+                    <img src={Share} className="Share_Icon" alt="Share Icon" />{" "}
+                    Share
+                  </span>
+                </div>
+                <div className="All_Comments_Container">
+                  <p>View 4 more comments</p>
+                  <div className="Individual_Comment_Container">
+                    <div className="Comments_Cotent">
+                      <img
+                        className="UserProfilePic"
+                        src={Logo}
+                        alt="Creator Profile Pic"
+                      />
+                      <div className="Comment">
+                        <p className="Person">Person Name</p>
+                        <p className="Person_Comment">
+                          Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is
+                          Cool Bro...
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="Individual_Comment_Container">
+                    <div className="Comments_Cotent">
+                      <img
+                        className="UserProfilePic"
+                        src={Logo}
+                        alt="Creator Profile Pic"
+                      />
+                      <div className="Comment">
+                        <p className="Person">Person Name</p>
+                        <p className="Person_Comment">
+                          Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is
+                          Cool Bro...
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="Add_Comment">
                   <img
-                    className="UserProfilePic"
+                    className="LoggedIn_User"
                     src={Logo}
                     alt="Creator Profile Pic"
                   />
-                  <div className="Comment">
-                    <p className="Person">Person Name</p>
-                    <p className="Person_Comment">
-                      Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is Cool
-                      Bro...
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="Individual_Comment_Container">
-                <div className="Comments_Cotent">
-                  <img
-                    className="UserProfilePic"
-                    src={Logo}
-                    alt="Creator Profile Pic"
+                  <input
+                    placeholder="Write A Comment"
+                    className="Comment_Input"
                   />
-                  <div className="Comment">
-                    <p className="Person">Person Name</p>
-                    <p className="Person_Comment">
-                      Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is Cool
-                      Bro...
-                    </p>
-                  </div>
                 </div>
               </div>
             </div>
-            <div className="Add_Comment">
-              <img
-                className="LoggedIn_User"
-                src={Logo}
-                alt="Creator Profile Pic"
-              />
-              <input placeholder="Write A Comment" className="Comment_Input" />
-            </div>
-          </div>
-        </div>
-      ))}
+          ))
+          .reverse()}
     </div>
   );
 }
