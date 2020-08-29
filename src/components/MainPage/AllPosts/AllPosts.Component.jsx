6b991bff6c78007ac0ba0d9a9c1f1db3ebecf673
AbhiTsong <abhisheksubba88@gmail.com>
2020-08-29 12:39:28 +0530

    Done with styling the all posts compopent

--- src/components/MainPage/AllPosts/AllPosts.Component.jsx
+++ src/components/MainPage/AllPosts/AllPosts.Component.jsx
@@ -8,6 +8,8 @@ import "./AllPosts.Styles.scss";
 
 // Static Imports
 import Logo from "../../../Assets/images/IMG-20190106-WA0001.jpg";
+import Comment from "../../../Assets/images/comments.png";
+import Share from "../../../Assets/images/share.png";
 
 function AllPostsComponent() {
   const dispatch = useDispatch();
@@ -27,8 +29,8 @@ function AllPostsComponent() {
 
   return (
     <div className="All_Post_Container">
-      {posts.map((post) => (
-        <div key={post.length} className="Single_Post_Container">
+      {posts.map((post, idx) => (
+        <div key={post+idx} className="Single_Post_Container">
           <div className="Image_Input_Container">
             <img
               className="UserProfilePic"
@@ -42,7 +44,9 @@ function AllPostsComponent() {
             </div>
           </div>
           <div className="Single_Post_Content">
-            <p className="Single_Post_Text">{post.description}</p>
+            <div className="Center_Post_Text">
+              <p className="Single_Post_Text">{post.description}</p>
+            </div>
           </div>
           <div className="Comments_Container">
             <div className="Reaction_Count">
@@ -55,11 +59,65 @@ function AllPostsComponent() {
               </div>
             </div>
             <div className="Like_Comment_Icons_Container">
-              <span role="img" aria-label="Like">&#128077; Like</span>
-              <span>Comment</span>
-              <span>Share</span>
+              <span role="img" aria-label="Like">
+                &#128077; Like
+              </span>
+              <span className="Icon_Container">
+                <img
+                  src={Comment}
+                  className="Comment_Icon"
+                  alt="Comment Icon"
+                />{" "}
+                Comment
+              </span>
+              <span className="Icon_Container">
+                <img src={Share} className="Share_Icon" alt="Share Icon" />{" "}
+                Share
+              </span>
             </div>
-            <div>Actual Comment Show Text</div>
+            <div className="All_Comments_Container">
+              <p>View 4 more comments</p>
+              <div className="Individual_Comment_Container">
+                <div className="Comments_Cotent">
+                  <img
+                    className="UserProfilePic"
+                    src={Logo}
+                    alt="Creator Profile Pic"
+                  />
+                  <div className="Comment">
+                    <p className="Person">Person Name</p>
+                    <p className="Person_Comment">
+                      Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is Cool
+                      Bro...
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="Individual_Comment_Container">
+                <div className="Comments_Cotent">
+                  <img
+                    className="UserProfilePic"
+                    src={Logo}
+                    alt="Creator Profile Pic"
+                  />
+                  <div className="Comment">
+                    <p className="Person">Person Name</p>
+                    <p className="Person_Comment">
+                      Yoo This Is Cool Bro.Yoo This Is Cool Bro.Yoo This Is Cool
+                      Bro...
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="Add_Comment">
+              <img
+                className="LoggedIn_User"
+                src={Logo}
+                alt="Creator Profile Pic"
+              />
+              <input placeholder="Write A Comment" className="Comment_Input" />
+            </div>
           </div>
         </div>
       ))}
